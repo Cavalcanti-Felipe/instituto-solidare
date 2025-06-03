039650aa3315715a0fae3499316a29e83cc2fc4a
caioguimaraesr <caioguimaraesrocha70@gmail.com>
2025-06-03 18:55:59 -0300

    finalizando a aba de avisos

--- instsoli/urls.py
+++ instsoli/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('avisos/', views.avisos_list, name='avisos'),
     path('avisos/criar/', views.criar_aviso, name='criar_aviso'),
     path('avisos/editar/<int:id>/', views.editar_aviso, name='editar_aviso'),
+    path('avisos/buscar/', views.lista_avisos, name='avisos'),
+    path('avisos/excluir/<int:aviso_id>/', views.excluir_aviso, name='excluir_aviso'),
 
     ## solicitacoes professor
     path('portal_professor/solicitacoes/', views.professor_solicitacoes, name='professor_solicitacoes'),
@@ -37,8 +39,6 @@ urlpatterns = [
 
     ## portal do aluno
     path('portal_aluno/', views.portal_aluno, name='portal_aluno'),
-    path('portal_aluno/avisos_aluno/', views.avisos_aluno, name='avisos_aluno'),
-    path('avisos/excluir/<int:aviso_id>/', views.excluir_aviso, name='excluir_aviso'),
 
     # solicitacoes
     path('portal_aluno/listar_solicitacoes', views.list_solicitacoes, name='listar_solicitacoes'),
--- instsoli/views.py
+++ instsoli/views.py
@@ -258,7 +258,23 @@ def excluir_aviso(request, aviso_id):
 
     return redirect('instsoli:avisos')
 
-
+def lista_avisos(request):
+    busca = request.GET.get('busca', '')
+    if busca:
+        avisos = Aviso.objects.filter(
+            titulo__icontains=busca
+        ) | Aviso.objects.filter(
+            mensagem__icontains=busca
+        ) | Aviso.objects.filter(
+            professor__username__icontains=busca
+        )
+    else:
+        avisos = Aviso.objects.all()
+    
+    return render(request, 'instsoli/pages/portal_professor/avisos/avisos.html', context={
+        'avisos': avisos,
+        'busca': busca
+    })
 
 
 
